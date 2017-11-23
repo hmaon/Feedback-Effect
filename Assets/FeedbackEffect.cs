f18be11c4c859c7bfbf7a5c866912a7153967728
Greg V <hmaon@bumba.net>
2017-11-23 11:02:24 -0500

    init via upload!
    nothing substantive yet

---
+++ Assets/FeedbackEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class FeedbackEffect : MonoBehaviour
+{
+    CommandBuffer _buf;
+    Camera _cam;
+    Material _mat;
+
+    private void Awake()
+    {
+        _mat = new Material(Shader.Find("Hidden/FeedbackEffect"));
+        int rtid = Shader.PropertyToID("_CopyOfBuffer");
+        _buf = new CommandBuffer();
+        _buf.name = "Feedback Effect";
+
+        _buf.GetTemporaryRT(rtid, -1, -1, 0, FilterMode.Bilinear, RenderTextureFormat.Default);
+        _buf.CopyTexture(BuiltinRenderTextureType.CameraTarget, rtid);
+
+        _cam = GetComponent<Camera>();
+    }
+
+    public void OnEnable()
+    {
+        _cam.AddCommandBuffer(CameraEvent.BeforeImageEffects, _buf);
+    }
+
+    public void OnDisable()
+    {
+        _cam.RemoveCommandBuffer(CameraEvent.BeforeImageEffects, _buf);
+    }
+}
